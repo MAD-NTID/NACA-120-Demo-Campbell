7d800e731ad61c9318bb2808fa5795ac2bbbc604
Kemoy Campbell <kscics@rit.edu>
2024-01-26 13:19:51 -0500

    array and method

---
+++ Array/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] names = new string[10];
+
+names[0] = "Panda";
+names[1] = "Freddy Kruger";
+names[2] = "Spy";
+
+Console.WriteLine(names[0]);
+
+for(int i = 0; i < names.Length; i++){
+    Console.WriteLine(names[i]);
+}
+
+foreach(string name in names)
+{
+    Console.WriteLine(name);
+}
+//for name in names: -- python
---
+++ methods/Program.cs
@@ -0,0 +1,26 @@
+﻿Greeting();
+Greeting("Alex");
+
+Console.WriteLine(Add(5,5));
+int sum = Add(5,10);
+Console.WriteLine(sum);
+
+int complex = Add(5,Add(3,20));
+Console.WriteLine(complex);
+
+
+/**
+This method output Hello Human!
+*/
+void Greeting(String name = "Human")
+{
+    Console.WriteLine($"Hello {name}!");
+    return ; //optional for return nothing
+}
+
+
+
+int Add(int number1, int number2)
+{
+    return number1 + number2;
+}
