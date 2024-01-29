b09ecd05887cf1f9260186ddf35f1a6f5473fa7e
Michael Berrios <mxbics@rit.edu>
2024-01-29 13:12:23 -0500

    Inclass code Unit 05, Exceptions and TryParse

---
+++ ExceptionsParsing/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// YES! try/catch
+int numberOfClasses = 0;
+
+while(true) {
+    Console.Write("Enter the number of classes you take this semester: ");
+
+    try
+    {
+        numberOfClasses = int.Parse(Console.ReadLine());
+
+        if(numberOfClasses < 1)
+            throw new FormatException();
+
+        break;
+    }
+    catch
+    {
+        Console.WriteLine("That's not a valid number of classes, duh");
+    }
+}
+
+Console.WriteLine($"You are taking {numberOfClasses} classes this semester");
---
+++ TryParsing/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int numberOfClasses = 0;
+
+while(true) 
+{
+    Console.Write("Enter the number of classes you take: ");
+
+    if(int.TryParse(Console.ReadLine(), out numberOfClasses) && numberOfClasses > 0) {
+        Console.WriteLine($"You are taking {numberOfClasses} this semester");
+    }
+    else {
+        Console.WriteLine("Invalid number of classes");
+    }
+}
+
+// while(true) 
+// {
+//     Console.Write("Enter the number of classes you take: ");
+
+//     if(int.TryParse(Console.ReadLine(), out int numberOfClasses) && numberOfClasses > 0) {
+//         Console.WriteLine($"You are taking {numberOfClasses} this semester");
+//     }
+//     else {
+//         Console.WriteLine("Invalid number of classes");
+//     }
+// }
