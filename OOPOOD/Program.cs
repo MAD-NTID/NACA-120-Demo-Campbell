0f5a3173da023b0a2928430d17ef2e3abbd4c1bf
Michael Berrios <mxbics@rit.edu>
2024-02-05 10:08:35 -0500

    Forgot to push Friday's Class 2/2/2024

---
+++ OOPOOD/Program.cs
@@ -0,0 +1,10 @@
+﻿Celebrity celebrityOne = new Celebrity();
+// celebrityOne.name = "Name Test";
+celebrityOne.SetName("");
+
+Console.WriteLine(celebrityOne.GetName());
+
+// You use a property as if it was a variable assignment
+celebrityOne.YearDeceased = "2ABC";
+
+Console.WriteLine(celebrityOne.YearDeceased);
